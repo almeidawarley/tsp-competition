41c170c3eb2ddd16acaecd67a265f3e3c5484b09
Warley Almeida <almeida.warley@outlook.com>
2021-06-26 01:06:37 -0300

    Added explanation to the test.py file + some solutions

--- test.py
+++ test.py
@@ -3,30 +3,41 @@ import env
 import os
 import sys
 
+# Load validation instance
 instance = mn.load_validation()
 instance = mn.adjust_instance(instance)
 
+# Set important parameters
 folder = 'solutions/'
 ref_objective = 5
 ref_percentage = 0.75
 ref_solution = ''
 
+# If there is an argument, output information for the solution specified in the argument
 if len(sys.argv) > 1:
     ref_solution = sys.argv[1]
     print('Listing information for solution {}'.format(ref_solution))
+# If there is not an argument, output information for solution with certain characteristics
 else:
     print('Listing all solutions with objective greater than {} and feasible at least {}%'
         .format(round(ref_objective, 2), round(100*ref_percentage, 0)))
 
+# Parse files in the solution directory
 for entry in os.listdir(folder):
     if '.out' in entry and ref_solution in entry:
+        
+        # Read solution from file
         solution = []
         path = os.path.join(folder, entry)
         with open(path) as content:
             for line in content:
                 line = line.replace(',', '')
                 solution.append(int(line))
+
+        # Check performance for 10^4 iterations
         objective, reward, percentage = mn.check_performance(instance, solution)
-        if objective >= ref_objective and percentage >= ref_percentage:
-            print('Solution in {} has objective {} and is feasible {}% of the time'
-                .format(path, round(objective, 2), round(100*percentage,0)))
+
+        # If solution has some characteristics or has been specificed, print information
+        if (objective >= ref_objective and percentage >= ref_percentage) or ref_solution != '' :
+            print('Solution: {} - Objective: {}, Feasibility: {}%, Reward: {}'
+                .format(path, round(objective, 2), round(100*percentage,0), round(reward, 2)))
